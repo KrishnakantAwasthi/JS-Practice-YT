acdbd98a4bb38be00ec8482e32f409f5887b487d
Krishnakant Awasthi <107775150+KrishnakantAwasthi@users.noreply.github.com>
2024-10-05 20:47:26 +0000

    update - 4

---
+++ 03_dataType.js
@@ -0,0 +1,37 @@
+"use strict";  // treat all js code as newer version
+
+// alert(3+3); -  We are using nodeJs , not Brower
+
+console.log(3+3) // code readablity should be high
+
+let name = "Raja"
+let age = 29
+let isLogIn = false;
+
+console.log([name , age , isLogIn]);
+
+// in JavaSript define 7 Primitive dataType
+
+// number   => 2 to power 53
+// bigint   => generally use in Stock Market websites
+// strig    => " "
+// boolean  => true/false
+// null     => standalone value
+// undefine => value is not assign
+// symbol   => unique
+
+// Object
+
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(isLogIn));
+console.log(typeof(null));           // null is object ( typeof) 
+console.log(typeof(undefine));       // undefined
+
+
+
+
+
+
+
+ 
---
+++ 04_DataType_Conversion.js
@@ -0,0 +1,37 @@
+// let score = "33"
+// let score = "33abc"
+// let score = "null"
+// let score = "undefined"
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+// "33"       => 33
+// "33abc"    => NaN
+// true => 1 false => 0
+
+let loggedIn = 1
+let boolIsLoggedIn = Boolean(loggedIn);
+console.log(boolIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false   , Empty
+// "hitesh" => true
+
+let someNumber = 33;
+
+let stringNumber = String (someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+ 
